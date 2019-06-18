fdbe8f107ca145d0203c928bfd7afccb09cf0d64
keisukeadachi <c3a578zx@gmail.com>
2019-06-18 22:43:34 +0900

    create gazou sample

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+    redirect_to :action => :index
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:image)
+  end
+
+end
